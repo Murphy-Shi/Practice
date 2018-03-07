616b66eb655fdcc996c71bac70c7d267a812e509
Murphy Shi <707444832@qq.com>
2018-03-07 08:38:01 +0800

    第三章流程控制语句

---
+++ Java/Practice/src/Main/Main.java
@@ -0,0 +1,18 @@
+package Main;
+
+import AprocessControl.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A();
+	}
+	
+	private static void A() {
+		Echo echo = new Echo();
+//		echo.Scanner();
+		echo.NumberFor();
+	}
+
+}
